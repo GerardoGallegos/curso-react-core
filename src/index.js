ada60907bc1b7d2f80abfd2edf3f643f55f4fd03
Gerardo Gallegos <puxerop@gmail.com>
2019-05-20 16:40:27 -0500

    Leccion 11

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const TarjetaFruta = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <h3>{ props.name }</h3>
-      <hr/>
-      <p>$ { props.price }</p>
-    </div>
-  )
+class TarjetaFruta extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      cantidad: 0
+    }
+  }
+
+
+  render () {
+    return (
+      <div>
+        <h3>{ this.props.name }</h3>
+        <p>Costo: $ {this.props.price}</p>
+        <div>Cantidad: { this.state.cantidad }</div>
+        <button
+          onClick={() => {
+            // X - Tratamos de actualizar el estado
+            this.setState({ cantidad: this.state.cantidad + 1 })
+          }}>
+      
+          Agregar
+        </button>
+        <hr />
+      </div>
+    )
+  }
 }
 
 const App = () => (
   <div>
     <TarjetaFruta name={'Sandia'} price={2.00} />
-    <TarjetaFruta name={'Naranja'} price={1.50} />
-    <TarjetaFruta name='Kiwi' price={3.30} />
+    <TarjetaFruta name={`Naranja`} price={1.50} />
+    <TarjetaFruta name={`Manzana`} price={1.50} />
   </div>
 )
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
